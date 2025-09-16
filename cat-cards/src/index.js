09b1c723cd9e5cdea6d466c5f95521e1fef70000
KristinaKolesnyk <kkristina.kolesnyk@gmail.com>
2025-09-16 16:24:20 -0400

    search field

--- cat-cards/src/index.js
+++ cat-cards/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import CardList from './CardList';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
 import {robots} from "./robots";
@@ -9,10 +9,10 @@ import {robots} from "./robots";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-<CardList robots={robots} />
+    <React.StrictMode>
+<App/>
 
-  </React.StrictMode>
+    </React.StrictMode>
 );
 
 
